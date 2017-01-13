2847e8508f9f19fb202c89b55bb1c52703fb797f
chulander <bryan.p.chu@gmail.com>
2017-01-13 02:08:19 -0500

    stashing for syncing

--- examples/safe.js
+++ examples/safe.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const { safe:{ assign, compare, get, } } = require(path.join(__dirname, '..', './lib/index.js'));
 
-const source = {
+var source = {
   package: {
     name: 'chutils',
   },
@@ -11,41 +11,43 @@ const source = {
     packageName: 'testPackage'
   },
   nested: {
+    testArray: ['randomString', {name:'randomLastName'}, 1, 2, 3],
     deep: {
       property: {
         value: 'test'
       }
-    }
-  }
+    },
+    func: function(){return 'function test'}
+  },
+
 }
 
 // assigns deeply nested properties even if intermediary properties do not exist
-var modifiedSource = assign(source, 'class.type.home', { year: 2017 })
-
+// var modifiedSource = assign(source, 'class.type.home', { year: 2017 })
+// console.log('source', source)
+// const newObj = assign(source, 'nested.deep', {value: 'bryan'});
+// console.log('newobj', newObj.nested);
+// const newObj2 = assign(source, 'nested.deep', {hello:'newBryan'});
+const newObj2 = assign(source, 'nested.deep.property.test', 'newBryan');
+// const newObj2 = assign(source, 'just.test', 'newBryan');
+// const newObj2 = assign(source, 'just.test', {value:'newBryan'});
+console.log('newObj2', newObj2.nested)
 /*
- {
- package: {
- name: 'chutils'
- },
- dependencies: {
- packageName: 'testPackage'
- },
- nested: {
- deep: {
- property: {
- value: 'test'
- }
- }
- },
- class: {
- type: {
- home: {
- year: 2017
- }
- }
- }
- }
+ modifiedSource { package: { name: 'chutils' },
+ dependencies: { packageName: 'testPackage' },
+ nested: { testArr: [ 'bryan', 1, 2, 3 ], deep: { property: {value: 'test'} } },
+ class: { type: { home: { year: 2017 } } } }
  */
+
+// var modifiedSourceOnArrayProp = assign(source, 'nested.testArray.retest', { year: 2017 })
+// console.log('modifiedSourceOnArrayProp', modifiedSourceOnArrayProp.nested);
+/*
+ modifiedSourceOnArrayProp { package: { name: 'chutils' },
+ dependencies: { packageName: 'testPackage' },
+ nested: { testArr: [ 'bryan', 1, 2, 3 ], deep: { newProp: { year: 2017 } } } }
+
+ */
+
 
 // compares deeply-nested properties that do not exist
 var testCompareFalse = compare(source, 'does.not.exist.property', 'notAProp') // false
@@ -61,6 +63,6 @@ var value1 = get(source, `nested.deep.property.value`); // test
 
 // gets the deeply-nested property value in different Object notations
 var value2 = get(source, `nested['deep'][property].value`); // test
-console.log('value2', value2)
+
 // gets the deeply-nested property value of undefined if deeply-nested property does not exists
 var value3 = get(source, `nested.deep.property.value.missing`); // undefined
--- test/safe.test.js
+++ test/safe.test.js
@@ -4,12 +4,12 @@ require('mocha');
 const path = require('path');
 const chai = require('chai');
 const expect = chai.expect;
-const {safe:{assign, compare,get}} = require(path.resolve(__dirname, '../lib'));
+const { safe:{ assign, compare, get } } = require(path.resolve(__dirname, '../lib'));
 const fs = require('fs');
 const bluebird = require('bluebird');
 
-describe('safe', function (){
-  const source = {
+describe('safe', function () {
+  var source = {
     package: {
       name: 'chutils',
     },
@@ -17,6 +17,7 @@ describe('safe', function (){
       packageName: 'testPackage'
     },
     nested: {
+      testArray: ['randomString', { name: 'randomLastName' }, 1, 2, 3],
       deep: {
         property: {
           value: 'test'
@@ -25,112 +26,150 @@ describe('safe', function (){
     }
   }
 
-  describe('assign', () =>{
-    it('Expects an function', () =>{
-      expect(typeof assign === 'function').to.be.true;
-    });
-    it('Expects a TypeError when invoked with argument "assignment" that is not an string', () =>{
-      try {
-        assign(source, null, 'anything')
-      }
-      catch (e) {
-        expect(e instanceof TypeError).to.be.true;
-      }
-    });
-    it('Expects the nested property to be added', () =>{
-      const deepProp = 'my.new.prop'
-      const newObj = assign(source, deepProp, 'newProp');
-      expect(newObj).to.have.deep.property(`${deepProp}`,'newProp')
+
+  describe('assign', () => {
+    describe('Error Handling', ()=>{
+      it('Expects an function', () => {
+        expect(typeof assign === 'function').to.be.true;
+      });
+      it('Expects a TypeError when invoked with argument "assignment" that is not an string', () => {
+        try {
+          assign(source, null, 'anything')
+        }
+        catch ( e ) {
+          expect(e instanceof TypeError).to.be.true;
+        }
+      });
     })
-    it('Expects the nested property to be added even if property is an object', () =>{
-      const deepProp = 'my.new.objectProp'
-      const newObj = assign(source, deepProp, {value:'objectProp'});
-      expect(newObj).to.have.deep.property(`${deepProp}.value`,'objectProp')
+    describe('when nested properties do not exist', ()=>{
+      const deepProp = 'my.new.prop';
+      const propValue = 'newProp';
+      it('Expects primitive values to be added', () => {
+        const newObj = assign(source, deepProp, propValue);
+        console.log('what is newObj.my.new.prop', newObj.my)
+        expect(newObj).to.have.deep.property(`${deepProp}`, propValue)
+      })
+      it('Expects objects to be added', () => {
+        const newObj = assign(source, deepProp, { value: propValue });
+        expect(newObj).to.have.deep.property(`${deepProp}.value`, propValue)
+      })
     })
-    it('Expects the nested property to be added with the correct value', () =>{
-      const deepProp = 'my.new.prop'
-      const newObj = assign(source, deepProp, 'newProp');
-      expect(newObj.my.new.prop).to.equal('newProp')
+    describe('when nested properties do exist', ()=>{
+      const deepProp = 'nested.deep.test';
+      const propValue = 'newProp';
+      describe('primitives', ()=>{
+        let newObj;
+        before(()=>{
+          newObj = assign(source, deepProp, propValue);
+        })
+        it('Expects primitive values to be added', () => {
+          expect(newObj.nested.deep.test).to.equal(propValue)
+        })
+        it('Expects nested properties along the path to still exist', ()=>{
+          expect(newObj).to.have.deep.property('nested.testArray')
+        })
+      })
+      describe('objects', ()=>{
+        console.log('what is source', source);
+        const newObj = assign(source, deepProp, {value: propValue});
+        it('Expects objects to be added', ()=>{
+          expect(newObj).to.have.deep.property(`${deepProp}.value`, propValue)
+          console.log('what is newObj.nested', newObj.nested)
+        })
+        it('Expects nested properties along the path to still exist', ()=>{
+          expect(newObj).to.have.deep.property('nested.deep.property.value', 'test')
+        })
+      })
+      describe('existing property is an array', ()=>{
+        it('Expects the nested property to be added to the end the array', () => {
+          const deepProp = 'nested.testArray.retest'
+          const newObj = assign(source, deepProp, { value: propValue });
+          const testArrayElement = newObj.nested.testArray.pop();
+          expect(testArrayElement).to.have.deep.property(`retest.value`, propValue)
+        })
+
+      })
     })
+
   })
-  describe('compare', () =>{
-    it('Expects to be a function', () =>{
+  describe('compare', () => {
+    it('Expects to be a function', () => {
       expect(typeof compare === 'function').to.be.true;
     });
-    it('Expects a TypeError when invoked with argument "object" that is not an Object', () =>{
+    it('Expects a TypeError when invoked with argument "object" that is not an Object', () => {
       try {
         compare(null, 'deep.property.check', 'anything')
       }
-      catch (e) {
+      catch ( e ) {
         expect(e instanceof TypeError).to.be.true;
       }
     });
-    it('Expects a TypeError when invoked with argument "assignment" that is not an String', () =>{
+    it('Expects a TypeError when invoked with argument "assignment" that is not an String', () => {
       try {
         compare(source, null, 'anything')
       }
-      catch (e) {
+      catch ( e ) {
         expect(e instanceof TypeError).to.be.true;
       }
     });
-    it('Expects a TypeError when invoked with argument "val" that is not an primitive', () =>{
+    it('Expects a TypeError when invoked with argument "val" that is not an primitive', () => {
       try {
-        compare(source, 'deep.property.check', {name: 'anything'})
+        compare(source, 'deep.property.check', { name: 'anything' })
       }
-      catch (e) {
+      catch ( e ) {
         expect(e instanceof TypeError).to.be.true;
       }
     });
-    it('Expects true if the nested property value equals the expected value', () =>{
+    it('Expects true if the nested property value equals the expected value', () => {
       expect(compare(source, 'nested.deep.property.value', 'test')).to.be.true
     })
-    it(`Expects true if the nested property value equals the expected value even if the argument "assignment" contains [,],", or ' object property notations`, () =>{
+    it(`Expects true if the nested property value equals the expected value even if the argument "assignment" contains [,],", or ' object property notations`, () => {
       expect(compare(source, `nested['deep']["property"].value`, 'test')).to.be.true
     })
-    it('Expects false if the nested property value does not equal the expected property value', () =>{
+    it('Expects false if the nested property value does not equal the expected property value', () => {
       expect(compare(source, 'nested.deep.property.value', 'falseyValue')).to.be.false
     })
-    it('Expects false if any member of the intermediary nested properties does not exist', () =>{
+    it('Expects false if any member of the intermediary nested properties does not exist', () => {
       expect(compare(source, 'does.not.exist.property', 'test')).to.be.false
     })
   })
-  describe('get', () =>{
-    it('Expects to be a function', () =>{
+  describe('get', () => {
+    it('Expects to be a function', () => {
       expect(typeof get === 'function').to.be.true;
     });
-    it('Expects a TypeError when invoked with argument "object" that is not an Object', () =>{
+    it('Expects a TypeError when invoked with argument "object" that is not an Object', () => {
       try {
         get(null, 'deep.property.check')
       }
-      catch (e) {
+      catch ( e ) {
         expect(e instanceof TypeError).to.be.true;
       }
     });
-    it('Expects a TypeError when invoked with argument "assignment" that is not an String', () =>{
+    it('Expects a TypeError when invoked with argument "assignment" that is not an String', () => {
       try {
         get(source, null, 'anything')
       }
-      catch (e) {
+      catch ( e ) {
         expect(e instanceof TypeError).to.be.true;
       }
     });
-    it('Expects the nested property value if it exists', () =>{
+    it('Expects the nested property value if it exists', () => {
       expect(get(source, 'nested.deep.property.value')).to.equal('test');
     })
-    it('Expects the nested property to have its own properties if it exists', () =>{
+    it('Expects the nested property to have its own properties if it exists', () => {
       // expect(get(source, 'nested.deep.property')).to.have.deep.property('source.nested.deep.property.value','test');
       expect(get(source, 'nested.deep.property')).to.be.an('object');
       expect(get(source, 'nested.deep.property')).to.have.property('value');
       expect(get(source, 'nested.deep.property')['value']).equal('test');
 
     })
-    it(`Expects the nested property value even if the argument "assignment" contains [,],", or ' object property notations`, () =>{
+    it(`Expects the nested property value even if the argument "assignment" contains [,],", or ' object property notations`, () => {
       expect(get(source, `nested['deep']["property"].value`)).equal('test');
     })
-    it('Expects undefined if the nested property value does not exist', () =>{
+    it('Expects undefined if the nested property value does not exist', () => {
       expect(get(source, 'does.not.exist.property')).to.be.an('undefined');
     })
-    it('Expects undefined if any member of the intermediary nested properties does not exist', () =>{
+    it('Expects undefined if any member of the intermediary nested properties does not exist', () => {
       expect(get(source, 'does.not.exist.property')).to.be.an('undefined');
     })
   })
