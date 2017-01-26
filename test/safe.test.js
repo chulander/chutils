84ec27a4e55f11fb5655974712456694046c9efa
chulander <bryan.p.chu@gmail.com>
2017-01-26 17:46:25 -0500

    updated safe assign and test case

--- test/safe.test.js
+++ test/safe.test.js
@@ -19,7 +19,7 @@ const fs = require('fs');
 const bluebird = require('bluebird');
 
 describe('safe', function () {
-  var source = {
+  const source = {
     package: {
       name: 'chutils',
     },
@@ -46,173 +46,162 @@ describe('safe', function () {
       },
     }
   }
+  const rootLayer = 'nested';
+  const propFirstLayer = `${rootLayer}.first_layer`;
+  const propSecondLayer = `${propFirstLayer}.second_layer`;
 
 
-  // describe('assign', () => {
-  //   const propValue = 'newProp';
-  //   const propObj = { primitive: propValue };
-  //
-  //   describe('sanity checks', () => {
-  //     it('Expects an function', () => {
-  //       expect(typeof assign === 'function').to.be.true;
-  //     });
-  //     it('Expects a TypeError when invoked with argument "assignment" that is not an string', () => {
-  //       try {
-  //         assign(source, null, 'anything')
-  //       }
-  //       catch ( e ) {
-  //         expect(e instanceof TypeError).to.be.true;
-  //       }
-  //     });
-  //   })
-  //   describe('obvious cases', () => {
-  //     describe('nested property do not exist', () => {
-  //       const deepProp = 'my.new.prop';
-  //       it('Expects primitive values to be added', () => {
-  //         const newObj = assign(source, deepProp, propValue);
-  //         expect(newObj).to.have.deep.property(`${deepProp}`, propValue)
-  //       })
-  //       it('Expects objects to be added', () => {
-  //         const newObj = assign(source, deepProp, { primitive: propValue });
-  //         expect(newObj).to.have.deep.property(`${deepProp}.value`, propValue)
-  //       })
-  //     })
-  //     describe('nested property exist', () => {
-  //       describe('same property level', () => {
-  //         describe('merging', () => {
-  //           const deepProp = 'nested.deep.test';
-  //           describe('primitives', () => {
-  //             let newObj;
-  //             before(() => {
-  //               newObj = assign(source, deepProp, propValue);
-  //             })
-  //             it('Expects primitive values to be added', () => {
-  //               expect(newObj.nested.deep.test).to.equal(propValue)
-  //             })
-  //             it('Expects nested property along the path to still exist', () => {
-  //               console.log('what is newObj', newObj);
-  //               expect(newObj).to.have.deep.property('nested.testArray[0]')
-  //             })
-  //             it('Expects nested property value along the path to still exist', () => {
-  //               expect(newObj).to.have.deep.property('nested.testArray[1].name', 'randomLastName')
-  //             })
-  //           })
-  //           describe('objects', () => {
-  //             const newObj = assign(source, deepProp, propObj);
-  //             it('Expects objects to be added', () => {
-  //               expect(newObj).to.have.deep.property(`${deepProp}.value`, propValue)
-  //             })
-  //             it('Expects nested property along the path to still exist', () => {
-  //               expect(newObj).to.have.deep.property('nested.deep.property.value')
-  //             })
-  //             it('Expects nested property value along the path to still exist', () => {
-  //               expect(newObj).to.have.deep.property('nested.deep.property.value', 'test')
-  //             })
-  //           })
-  //         })
-  //
-  //
-  //       })
-  //       describe('deeper property level', () => {
-  //         const deepProp = 'nested.deep.property.deeperProperty';
-  //         describe('objects', () => {
-  //           let newObj;
-  //           before(() => {
-  //             newObj = assign(source, deepProp, propObj);
-  //           })
-  //           it('Expects object assignments to merge with existing properties', () => {
-  //             expect(newObj.nested.deep.property.deeperProperty.value).to.equal(propValue)
-  //           })
-  //           it('Expects nested property along the path to still exist', () => {
-  //             expect(newObj).to.have.deep.property('nested.deep.property')
-  //           })
-  //           it('Expects nested property value along the path to still exist', () => {
-  //             expect(newObj).to.have.deep.property('nested.deep.property.value', 'test')
-  //           })
-  //         })
-  //       })
-  //     })
-  //   })
-  //   describe('edge cases', () => {
-  //     describe('nested property do exist', () => {
-  //       describe('overwriting', () => {
-  //         describe('same property level', () => {
-  //           const deepProp = 'nested.deep.property';
-  //           it('Expects adding a primitive value to result in an error', () => {
-  //             try {
-  //               assign(source, deepProp, propValue);
-  //             }
-  //             catch ( e ) {
-  //               expect(e instanceof Error).to.be.true;
-  //             }
-  //           })
-  //           it('Expects adding a object to result in an error', () => {
-  //             try {
-  //               assign(source, deepProp, propObj);
-  //             }
-  //             catch ( e ) {
-  //               expect(e instanceof Error).to.be.true;
-  //             }
-  //           })
-  //         })
-  //         describe('deeper property level', () => {
-  //           const deepProp = 'nested.deep.property.deeperProperty';
-  //           let newObj;
-  //           before(() => {
-  //             newObj = assign(source, deepProp, propObj);
-  //           })
-  //           it('Expects adding a primitive value to result in an error', () => {
-  //             try {
-  //               assign(source, deepProp, propValue);
-  //             }
-  //             catch ( e ) {
-  //               expect(e instanceof Error).to.be.true;
-  //             }
-  //           })
-  //           it('Expects object assignments to merge with existing properties', () => {
-  //             expect(newObj.nested.deep.property.deeperProperty.value).to.equal(propValue)
-  //           })
-  //           it('Expects nested property along the path to still exist', () => {
-  //             expect(newObj).to.have.deep.property('nested.deep.property')
-  //           })
-  //           it('Expects nested property value along the path to still exist', () => {
-  //             expect(newObj).to.have.deep.property('nested.deep.property.value', 'test')
-  //           })
-  //         })
-  //       })
-  //     })
-  //   })
-  //   describe('opinionated cases', () => {
-  //     describe('arrays', () => {
-  //       const deepProp = 'nested.testArray.retest';
-  //       const arrayLength = source.nested.testArray.length;
-  //       describe('existing property is an array', () => {
-  //         let newTestObj;
-  //         let newTestPrimitive;
-  //         before(() => {
-  //           newTestObj = assign(source, deepProp, propObj);
-  //           newTestPrimitive = assign(source, deepProp, propValue);
-  //         })
-  //         it('Expects the array length to increase after nested object is assigned', () => {
-  //           const newArrayLength = newTestObj.nested.testArray.length;
-  //           expect(newArrayLength - arrayLength).to.equal(1);
-  //         })
-  //         it('Expects the last element of the array to be the correct object ', () => {
-  //           const testArrayElement = newTestObj.nested.testArray.pop();
-  //           expect(testArrayElement).to.have.deep.property(`retest.value`, propValue)
-  //         })
-  //         it('expects the array length to increase after nested primitive value is assigned', () => {
-  //           const newArrayLength = newTestPrimitive.nested.testArray.length;
-  //           expect(newArrayLength - arrayLength).to.equal(1);
-  //         })
-  //         it('Expects the last element of the array to be the correct primitive value ', () => {
-  //           const testArrayElement = newTestPrimitive.nested.testArray.pop();
-  //           expect(testArrayElement).to.have.deep.property(`retest`, propValue)
-  //         })
-  //       })
-  //     })
-  //   })
-  // })
+  describe('assign', () => {
+    const propValue = 'assignPropValue';
+    const propObj = { primitive: propValue };
+    describe('sanity checks', () => {
+      it('Expects an function', () => {
+        expect(typeof assign === 'function').to.be.true;
+      });
+      it('Expects a TypeError when invoked with argument "assignment" that is not an string', () => {
+        try {
+          assign(source, null, 'anything')
+        }
+        catch ( e ) {
+          expect(e instanceof TypeError).to.be.true;
+        }
+      });
+    })
+    describe('obvious cases', () => {
+      describe('nested property do not exist', () => {
+        const deepProp = 'my.new.prop';
+        it('Expects primitive values to be added', () => {
+          const newObj = assign(source, deepProp, propValue);
+          expect(newObj).to.have.deep.property(`${deepProp}`, propValue)
+        })
+        it('Expects objects to be added', () => {
+          const newObj = assign(source, deepProp, propObj);
+          expect(newObj).to.have.deep.property(`${deepProp}.${Object.keys(propObj)[0]}`, `${propObj[Object.keys(propObj)[0]]}`)
+        })
+        it('Expects array to be added', () => {
+          const newObj = assign(source, deepProp, [propObj]);
+          expect(newObj).to.have.deep.property(`${deepProp}[0].${Object.keys(propObj)[0]}`, `${propObj[Object.keys(propObj)[0]]}`)
+        })
+        it('Expects function to be added', () => {
+          const newObj = assign(source, deepProp, function () {return propObj});
+          const testFunc = get(newObj, deepProp);
+          expect(testFunc).to.be.an('function')
+          // expect(testFunc).to.be.an('object');
+          // expect(testFunc).to.have.deep.property('primitive', propValue)
+        })
+      })
+      describe('nested property exist', () => {
+        describe('property level: first_layer', () => {
+          const deepProp = `${propFirstLayer}.deepProp`;
+          describe('primitives', () => {
+            let newObj;
+            before(() => {
+              newObj = assign(source, deepProp, propValue);
+            })
+            it('Expects primitive values to be added', () => {
+              expect(newObj.nested.first_layer.deepProp).to.equal(propValue)
+            })
+            it('Expects nested property along the path to still exist', () => {
+              console.log('what is newObj', newObj);
+              expect(newObj).to.have.deep.property('nested.first_layer.arr')
+            })
+            it('Expects nested property value along the path to still exist', () => {
+              expect(newObj).to.have.deep.property('nested.first_layer.arr[1].name', 'randomLastName')
+            })
+          })
+          describe('objects', () => {
+            const newObj = assign(source, deepProp, propObj);
+            it('Expects objects to be added', () => {
+              expect(newObj).to.have.deep.property(`${deepProp}.primitive`, propValue)
+            })
+            it('Expects nested property along the path to still exist', () => {
+              expect(newObj).to.have.deep.property('nested.first_layer.primitive')
+            })
+            it('Expects nested property value along the path to still exist', () => {
+              expect(newObj).to.have.deep.property('nested.first_layer.primitive', 'first_layer_primitive')
+            })
+          })
+        })
+      })
+    })
+    describe('edge cases', () => {
+      describe('nested property do exist', () => {
+        describe('property level: first_layer', () => {
+          it('Expects adding a primitive value to result in an error', () => {
+            try {
+              assign(source, propFirstLayer, propObj);
+            }
+            catch ( e ) {
+              expect(e instanceof Error).to.be.true;
+            }
+          })
+          it('Expects adding a object to result in an error', () => {
+            try {
+              assign(source, propFirstLayer, propObj);
+            }
+            catch ( e ) {
+              expect(e instanceof Error).to.be.true;
+            }
+          })
+        })
+        describe('property level: second_layer', () => {
+          let newObj;
+          const deepProp = `${propSecondLayer}.deepProp`;
+          before(() => {
+            newObj = assign(source, deepProp, propValue);
+          })
+          it('Expects adding a primitive value to result in an error', () => {
+            try {
+              assign(source, propSecondLayer, propValue);
+            }
+            catch ( e ) {
+              expect(e instanceof Error).to.be.true;
+            }
+          })
+          it('Expects object assignments to merge with existing properties', () => {
+            expect(get(newObj, `${deepProp}`)).to.equal(propValue)
+          })
+          it('Expects nested property along the path to still exist', () => {
+            expect(newObj).to.have.deep.property(`${propFirstLayer}.${Object.keys(propObj)[0]}`)
+          })
+          it('Expects nested property value along the path to still exist', () => {
+            expect(newObj).to.have.deep.property(`${propSecondLayer}.${Object.keys(propObj)[0]}`, 'second_layer_primitive')
+          })
+        })
+      })
+    })
+    describe('opinionated cases', () => {
+      describe('arrays', () => {
+        const deepProp = `${propFirstLayer}.arr`;
+        const arrayLength = get(source, `${deepProp}`).length;
+        describe('existing property is an array', () => {
+          let newTestObj;
+          let newTestPrimitive;
+          before(() => {
+            console.log('what is deepProp', deepProp);
+            newTestObj = assign(source, deepProp, propObj);
+            newTestPrimitive = assign(source, deepProp, propValue);
+          })
+          it('Expects the array length to increase after nested object is assigned', () => {
+            const newArrayLength = get(newTestObj, `${deepProp}`).length;
+            expect(newArrayLength - arrayLength).to.equal(1);
+          })
+          it('Expects the last element of the array to be the correct object ', () => {
+            const testArrayElement = get(newTestObj, `${deepProp}`).pop();
+            expect(testArrayElement).to.have.deep.property(`${Object.keys(propObj)[0]}`, propValue)
+          })
+          it('expects the array length to increase after nested primitive value is assigned', () => {
+            const newArrayLength = get(newTestPrimitive, `${deepProp}`).length;;
+            expect(newArrayLength - arrayLength).to.equal(1);
+          })
+          it('Expects the last element of the array to be the correct primitive value ', () => {
+            const testArrayElement = get(newTestPrimitive, `${deepProp}`).pop();
+            expect(testArrayElement).to.equal(propValue)
+          })
+        })
+      })
+    })
+  })
   describe('valueCompare', () => {
     it('Expects to be a function', () => {
       expect(typeof valueCompare === 'function').to.be.true;
@@ -254,18 +243,18 @@ describe('safe', function () {
       expect(valueCompare(source, 'does.not.exist.property', 'test')).to.be.false
     })
   })
-  // describe('typeCompare', () => {
-  //
-  //   it('Expects type to be a "string"', () => {
-  //     const testObj = assign(source, 'nested.deep.property.value', 'test');
-  //     expect(typeCompare(testObj, 'nested.deep.property.value', 'string')).to.be.true;
-  //   })
-  //   it('Expects type to be a "null"', () => {
-  //     const testObj = assign(source, 'nested.deep.property.value', null);
-  //     expect(typeCompare(testObj, 'nested.deep.property.value', 'null')).to.be.true;
-  //   })
-  //
-  // })
+  describe('typeCompare', () => {
+
+    it('Expects type to be a "string"', () => {
+      expect(typeCompare(source, `${propFirstLayer}.primitive`, 'string')).to.be.true;
+    })
+    it('Expects type to be a "null"', () => {
+      const deepProp = `${propFirstLayer}.typeCompareNull`;
+      const testObj = assign(source, deepProp, null);
+      expect(typeCompare(testObj, deepProp, 'null')).to.be.true;
+    })
+
+  })
   describe('get', () => {
     const _root = 'nested';
     const _primitive = 'primitive';
@@ -364,9 +353,9 @@ describe('safe', function () {
         const notations_second_layer_object = `nested.first_layer['second_layer']["obj"]`
         it(`Expects "${notations_second_layer_object}" to be an object with the property "${_primitive}" value of ${_primitive}`, () => {
           expect(get(source, notations_second_layer_object)).to.be.an('object');
-          expect(get(source, notations_second_layer_object)).to.have.property(_primitive,`${_second_layer}_object_${_primitive}`);
+          expect(get(source, notations_second_layer_object)).to.have.property(_primitive, `${_second_layer}_object_${_primitive}`);
         })
-        it(`Expects .dot notation using numbers to be handled`,()=>{
+        it(`Expects .dot notation using numbers to be handled`, () => {
           console.log('what is second_layer_prop_number_as_key', second_layer_prop_number_as_key)
           expect(get(source, second_layer_prop_number_as_key)).to.equal('primitive_number_as_key')
         })
